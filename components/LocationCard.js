3383c91ede697b0f33e00ae4e3c7090b64f4e445
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-06-03 18:52:52 +0100

    Added in error handling to search field

--- components/LocationCard.js
+++ components/LocationCard.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function LocationCard(props) {
   const { name, grid, region, type } = props;
+
   return (
     <span>
       <h4>{name}</h4>
--- pages/location.js
+++ pages/location.js
@@ -1,24 +1,32 @@
 import { useRouter } from "next/router";
 import React, { useState, useEffect } from "react";
 import location_data from "../location_data";
+import LocationCard from "../components/LocationCard";
 
 export default function location() {
   const router = useRouter();
   const { searchQuery } = router.query;
   const [location, setLocation] = useState({});
-  useEffect(
-    () =>
-      setLocation(
-        location_data.find((location) => {
-          return location.name == searchQuery;
-        }),
-      ),
-    [],
-  );
-  return (
-    <div>
-      <h1>Location page</h1>
-      <h4>{location.name}</h4>
-    </div>
-  );
+  useEffect(() => {
+    setLocation(
+      location_data.find((location) => {
+        return location.name == searchQuery;
+      }),
+    );
+  }, []);
+
+  if (!location) {
+    return <h1>Oh no</h1>;
+  } else {
+    return (
+      <div>
+        <LocationCard
+          name={location.name}
+          grid={location.grid}
+          region={location.region}
+          type={location.type}
+        />
+      </div>
+    );
+  }
 }
